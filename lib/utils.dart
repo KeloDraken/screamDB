88e3e5da51bc1ba8763bbd743eb2fcc812d374dd
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-10-19 11:30:39 +0200

    Make get user home dir function private

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
-String getUserHomeDir() {
+String _getUserHomeDir() {
   Map<String, String> envVars = Platform.environment;
   dynamic home = envVars['UserProfile'];
   return home;
 }
 
 void createScreamDirectory() async {
-  String userHomeDir = getUserHomeDir();
+  String userHomeDir = _getUserHomeDir();
   final bool pathExists = await Directory(userHomeDir).exists();
-  if(pathExists) return;
+  if (pathExists) return;
   await Directory("$userHomeDir/.screamDB").create(recursive: true);
 }
