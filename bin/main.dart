a5b8b21f7a5000cba9b98e34953e0c49a485bb5c
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-10-19 11:00:54 +0200

    Can create .screamDB/ in user home dir

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,5 @@
 import 'package:scream_db/utils.dart';
 
 void main(List<String> arguments) {
-  String userHomeDir = getUserHomeDir();
-  print(userHomeDir);
+  createScreamDirectory();
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,8 +1,12 @@
-import 'dart:io' show Platform;
+import 'dart:io';
 
 String getUserHomeDir() {
   dynamic home;
   Map<String, String> envVars = Platform.environment;
   home = envVars['UserProfile'];
   return home;
+}
+
+void createScreamDirectory() async {
+  await Directory("${getUserHomeDir()}/.screamDB").create(recursive: true);
 }
