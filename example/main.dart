4038cdb2556a2a47e39bd52b42b7d001945264d2
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-10-19 13:08:58 +0200

    Create example usage

--- example/main.dart
+++ example/main.dart
@@ -3,5 +3,23 @@ import 'package:scream_db/db.dart';
 void main(List<String> args) {
   ScreamDB db = ScreamDB("test");
 
-  
+  // Table structure
+  Map<String, dynamic> table = {
+    "scream_name": "this_is_the_table_name",
+    "id": int,
+    "name": String,
+  };
+
+  // Save the table to database
+  db.createTable(table);
+
+  // Get all records from the table
+  List<Map<String, dynamic>> data =
+      db.getAllFromTable("this_is_the_table_name");
+
+  // Delete all records from the table
+  Future<bool> success = db.deleteAllFromTable("this_is_the_table_name");
+
+  // Drop table
+  db.dropTable("this_is_the_table_name");
 }
--- lib/db.dart
+++ lib/db.dart
@@ -1,5 +1,48 @@
+import 'dart:io';
+
+import 'package:scream_db/constants.dart';
+import 'package:scream_db/utils.dart';
+
 class ScreamDB {
   String name = "";
 
-  ScreamDB(this.name);
+  ScreamDB(String name) {
+    try {
+      this.name = _setUpName(name);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void createTable(Map<String, dynamic> table) {}
+
+  void dropTable(String tableName) {}
+
+  List<Map<String, dynamic>> getAllFromTable(String tableName) {
+    return [{}];
+  }
+
+  Future<bool> deleteAllFromTable(String tableName) {
+    return Future.value(true);
+  }
+
+  String _setUpName(String name) {
+    String db = "";
+    _checkDb(name).then((value) => db = value);
+    return db;
+  }
+
+  Future<String> _checkDb(String dbName) async {
+    final String userHomeDir = getUserHomeDir();
+    String? dbName = stdin.readLineSync();
+    String sep = Platform.pathSeparator;
+
+    final String dbDir = "$userHomeDir$sep$screamDb$sep$dbName";
+
+    bool pathExists = false;
+    await Directory(dbDir).exists().then((value) => pathExists = value);
+
+    if (pathExists) return Future.value(dbDir);
+    throw NoSuchMethodError;
+  }
 }
